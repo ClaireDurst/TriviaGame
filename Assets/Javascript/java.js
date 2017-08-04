00944859e8840489ff434f1e36190517fbca0d8d
Claire Durst <clairedurst@gmail.com>
2017-08-03 22:22:56 -0400

    homework

---
+++ Assets/Javascript/java.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+var count=30;
+
+var counter=setInterval(timer, 1000);
+
+var countee=setTimeout(timer, 1000);
+
+
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+   alert("Time is UP!!");
+   $("button").click()
+  }
+
+  document.getElementById("timer").innerHTML=count + " secs";
+
+  //Do code for showing the number of seconds here
+}
+    $("button").click(function(){
+        $(".Tally").show();
+    });
+
+    $("button").click(function(){
+        $("#starter").hide();
+    });
+
+    $("button").click(function(){
+    $("#results").hide();
+    });
+
+
+    $("button").click(function(){
+    $("#timer").setTimeout();
+    });
+
+
+	var answeredWrong = 7;
+  var answeredRight = 0;
+  var questionsRemaining = 7;
+
+
+$("button").click(function(){         
+answeredRight = 0;          
+for(var i = 1; i <= 7; i++) {
+  var radios = document.getElementsByName('group'+i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if(radio.value == "correct" && radio.checked) {
+      answeredRight++;
+      answeredWrong--;
+      questionsRemaining--;
+
+    }
+  
+  }
+ } 
+
+ if(radio.value == "wrong" && radio.checked) {
+      answeredWrong++; 
+    }
+
+  $('#Incorrect').text(answeredWrong);
+
+  $('#Correct').text(answeredRight);
+
+  $('#QuestionsRemaining').text(questionsRemaining);
+
+
+  });
+
+
+
+  });
+
