753865a845c584b17e2311f7f79936893ad6bb71
Claire <clairedurst@gmail.com>
2017-10-24 00:05:37 -0400

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.sendFile('/index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
